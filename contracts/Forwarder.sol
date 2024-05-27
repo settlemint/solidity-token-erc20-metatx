ba7682ee40241b92e8fdadd5e8705a7e2d5c58dc
bl0up <ambroise.m2@gmail.com>
2024-05-27 19:56:54 +0200

    fix: foundry config

--- contracts/Forwarder.sol
+++ contracts/Forwarder.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 // SettleMint.com
 
-pragma solidity 0.8.24;
+pragma solidity 0.8.26;
 
-import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import { EIP712 } from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
-import { ERC165 } from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
 /**
  * @dev Simple minimal forwarder to be used together with an ERC2771 compatible contract. See {ERC2771Context}.
@@ -26,7 +26,9 @@ contract Forwarder is EIP712, ERC165 {
     event MetaTransactionExecuted(address indexed from, address indexed to);
 
     bytes32 private constant _TYPEHASH =
-        keccak256("ForwardRequest(address from,address to,uint256 value,uint256 gas,uint256 nonce,bytes data)");
+        keccak256(
+            "ForwardRequest(address from,address to,uint256 value,uint256 gas,uint256 nonce,bytes data)"
+        );
 
     mapping(address => uint256) private _nonces;
 
@@ -42,9 +44,22 @@ contract Forwarder is EIP712, ERC165 {
         return address(this);
     }
 
-    function verify(ForwardRequest calldata req, bytes calldata signature) public view returns (bool) {
+    function verify(
+        ForwardRequest calldata req,
+        bytes calldata signature
+    ) public view returns (bool) {
         address signer = _hashTypedDataV4(
-            keccak256(abi.encode(_TYPEHASH, req.from, req.to, req.value, req.gas, req.nonce, keccak256(req.data)))
+            keccak256(
+                abi.encode(
+                    _TYPEHASH,
+                    req.from,
+                    req.to,
+                    req.value,
+                    req.gas,
+                    req.nonce,
+                    keccak256(req.data)
+                )
+            )
         ).recover(signature);
         return _nonces[req.from] == req.nonce && signer == req.from;
     }
@@ -52,16 +67,17 @@ contract Forwarder is EIP712, ERC165 {
     function execute(
         ForwardRequest calldata req,
         bytes calldata signature
-    )
-        public
-        payable
-        returns (bool, bytes memory)
-    {
-        require(verify(req, signature), "MinimalForwarder: signature does not match request");
+    ) public payable returns (bool, bytes memory) {
+        require(
+            verify(req, signature),
+            "MinimalForwarder: signature does not match request"
+        );
         _nonces[req.from] = req.nonce + 1;
 
-        (bool success, bytes memory returndata) =
-            req.to.call{ gas: req.gas, value: req.value }(abi.encodePacked(req.data, req.from));
+        (bool success, bytes memory returndata) = req.to.call{
+            gas: req.gas,
+            value: req.value
+        }(abi.encodePacked(req.data, req.from));
         // Validate that the relayer has sent enough gas for the call.
         // See https://ronan.eth.link/blog/ethereum-gas-dangers/
         assert(gasleft() > req.gas / 63);
@@ -70,7 +86,11 @@ contract Forwarder is EIP712, ERC165 {
         return (success, returndata);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165) returns (bool) {
-        return interfaceId == type(EIP712).interfaceId || super.supportsInterface(interfaceId);
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(ERC165) returns (bool) {
+        return
+            interfaceId == type(EIP712).interfaceId ||
+            super.supportsInterface(interfaceId);
     }
 }
--- contracts/GenericTokenMeta.sol
+++ contracts/GenericTokenMeta.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 // SettleMint.com
 
-pragma solidity 0.8.24;
+pragma solidity 0.8.26;
 
-import { ERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import { ERC20Pausable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { ERC2771Context, Context } from "@openzeppelin/contracts/metatx/ERC2771Context.sol";
-import { ERC165 } from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import {ERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC2771Context, Context} from "@openzeppelin/contracts/metatx/ERC2771Context.sol";
+import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 
 /**
  * @title GenericTokenMeta
@@ -23,7 +23,14 @@ import { ERC165 } from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
  *  For demonstrative purposes, 1 million GTM tokens are pre-mined to the address
  *  deploying this contract.
  */
-contract GenericTokenMeta is ERC20, ERC20Burnable, ERC165, ERC2771Context, ERC20Pausable, Ownable {
+contract GenericTokenMeta is
+    ERC20,
+    ERC20Burnable,
+    ERC165,
+    ERC2771Context,
+    ERC20Pausable,
+    Ownable
+{
     constructor(
         string memory name_,
         string memory symbol_,
@@ -94,11 +101,21 @@ contract GenericTokenMeta is ERC20, ERC20Burnable, ERC165, ERC2771Context, ERC20
         _burn(_msgSender(), amount);
     }
 
-    function _msgSender() internal view override(Context, ERC2771Context) returns (address sender) {
+    function _msgSender()
+        internal
+        view
+        override(Context, ERC2771Context)
+        returns (address sender)
+    {
         sender = ERC2771Context._msgSender();
     }
 
-    function _msgData() internal view override(Context, ERC2771Context) returns (bytes calldata) {
+    function _msgData()
+        internal
+        view
+        override(Context, ERC2771Context)
+        returns (bytes calldata)
+    {
         return ERC2771Context._msgData();
     }
 
@@ -116,17 +133,31 @@ contract GenericTokenMeta is ERC20, ERC20Burnable, ERC165, ERC2771Context, ERC20
      * - when `to` is zero, `amount` of `from`'s tokens will be burned.
      * - `from` and `to` are never both zero.
      */
-    function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Pausable) {
+    function _update(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Pausable) {
         super._update(from, to, amount);
     }
 
-    function _contextSuffixLength() internal view override(Context, ERC2771Context) returns (uint256) {
+    function _contextSuffixLength()
+        internal
+        view
+        override(Context, ERC2771Context)
+        returns (uint256)
+    {
         return super._contextSuffixLength();
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165) returns (bool) {
-        return interfaceId == type(IERC20).interfaceId || interfaceId == type(ERC20Burnable).interfaceId
-            || interfaceId == type(ERC20Pausable).interfaceId || interfaceId == type(ERC2771Context).interfaceId
-            || super.supportsInterface(interfaceId); // ERC165, AccessControl
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(ERC165) returns (bool) {
+        return
+            interfaceId == type(IERC20).interfaceId ||
+            interfaceId == type(ERC20Burnable).interfaceId ||
+            interfaceId == type(ERC20Pausable).interfaceId ||
+            interfaceId == type(ERC2771Context).interfaceId ||
+            super.supportsInterface(interfaceId); // ERC165, AccessControl
     }
 }
