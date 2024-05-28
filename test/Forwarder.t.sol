c6f504923e5c961b70d9c2177dfafb318e40a0fd
bl0up <ambroise.m2@gmail.com>
2024-05-28 17:44:03 +0200

    feat: converage

--- test/Forwarder.t.sol
+++ test/Forwarder.t.sol
@@ -25,6 +25,68 @@ contract ForwarderTest is Test {
         token.mint(signer, 20);
     }
 
+    function testNonce() public view {
+        uint256 nonce = forwarder.getNonce(signer);
+        assertEq(nonce, 0);
+    }
+
+    function testVerify() public view {
+        bytes32 domainSeparator = keccak256(
+            abi.encode(
+                keccak256(
+                    "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+                ),
+                keccak256(bytes("MinimalForwarder")), // Name
+                keccak256(bytes("0.0.1")), // Version
+                block.chainid,
+                address(forwarder)
+            )
+        );
+
+        bytes4 transferSelector = bytes4(
+            keccak256("transfer(address,uint256)")
+        );
+        bytes memory data = abi.encodeWithSelector(transferSelector, owner, 10);
+
+        uint256 nonce = forwarder.getNonce(signer);
+
+        Forwarder.ForwardRequest memory req = Forwarder.ForwardRequest({
+            from: signer,
+            to: address(token),
+            value: 0,
+            gas: 300_000,
+            nonce: nonce,
+            data: data
+        });
+
+        bytes32 structHash = keccak256(
+            abi.encode(
+                keccak256(
+                    "ForwardRequest(address from,address to,uint256 value,uint256 gas,uint256 nonce,bytes data)"
+                ),
+                req.from,
+                req.to,
+                req.value,
+                req.gas,
+                req.nonce,
+                keccak256(req.data)
+            )
+        );
+        bytes32 digest = keccak256(
+            abi.encodePacked("\x19\x01", domainSeparator, structHash)
+        );
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, digest);
+
+        // Adjust the `v` value if necessary
+        if (v == 0 || v == 1) {
+            v += 27;
+        }
+
+        bytes memory signature = abi.encodePacked(r, s, v);
+        forwarder.verify(req, signature);
+    }
+
     function testMetaTransactionExecution() public {
         bytes32 domainSeparator = keccak256(
             abi.encode(
