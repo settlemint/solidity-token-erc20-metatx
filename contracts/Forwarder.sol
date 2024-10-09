3aeeb4f706d50a33181f4bb8be4d37a55048e0d3
Roderik van der Veer <roderik@settlemint.com>
2024-10-09 07:47:35 +0200

    fix: small issues on the ci

--- contracts/Forwarder.sol
+++ contracts/Forwarder.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: FSL-1.1-MIT
 // OpenZeppelin Contracts (last updated v5.0.0) (metatx/ERC2771Forwarder.sol)
 
-pragma solidity 0.8.24;
+pragma solidity ^0.8.24;
 
 import { ERC2771Forwarder } from "@openzeppelin/contracts/metatx/ERC2771Forwarder.sol";
 
